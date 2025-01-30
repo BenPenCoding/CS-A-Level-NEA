d1b2548131be5ebdc32f642f914624ed81369707
BenPenCoding <benpennycook@outlook.com>
2025-01-30 20:46:54 +0000

    Reformatted to a .NET framework app instead of a .NET Core app.

--- NEA MAIN/App.xaml.cs
+++ NEA MAIN/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace NEA_MAIN
@@ -10,5 +14,4 @@ namespace NEA_MAIN
     public partial class App : Application
     {
     }
-
 }
