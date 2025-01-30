1174cb0a443d3556f8ceac57ee2a7586ac9569b0
BenPenCoding <benpennycook@outlook.com>
2025-01-30 00:08:06 +0000

    Created VS project.

---
+++ NEA MAIN/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NEA_MAIN
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
