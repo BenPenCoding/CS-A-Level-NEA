16231b8f3f04979fd9c4ca6c9547ba64c83abc5c
Ben Pennycook <benbyc@outlook.com>
2025-02-07 12:36:48 +0000

    Updated Move class

--- Technical solution/ChessEngine.py
+++ Technical solution/ChessEngine.py
@@ -1,15 +1,20 @@
 import pygame as pg
 
-
-
 class Piece():
-  def __init__(self, pieceType, column, file):
-    
+	def __init__(self, pieceType, column, file):
+		self.pieceType = pieceType
+		self.column = column
+		self.file = file
+		
 
 class Move():
 
-  def __init__(self, startPiece, endPiece):
-    
-    self.startPiece = startPiece
-    self.endPieve = startPiece
-    self.startPieceNotation = 
+	def __init__(self, startPiece, endPiece):
+		pieceTypeDict ={"Knight":"N", "Queen": "Q", "King": "K", "Bishop": "B", "Pawn":"", "Empty":"", "Rook":"R"}
+		self.startPiece = startPiece
+		self.endPiece = endPiece
+		self.startPieceNotation = pieceTypeDict[self.startPiece]
+		self.endPieceNotation = pieceTypeDict[self.endPiece]
+	
+	
+		
