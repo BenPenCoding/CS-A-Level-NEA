fd3f680258d377c3240bd0c8a7d9efba0ca16a7a
Ben Pennycook <139886382+BenPenCoding@users.noreply.github.com>
2025-02-07 11:18:56 +0000

    Update ChessEngine.py

--- Technical solution/ChessEngine.py
+++ Technical solution/ChessEngine.py
@@ -1,1 +1,15 @@
-#TBC
+import pygame as pg
+
+
+
+class Piece():
+  def __init__(self, pieceType, column, file):
+    
+
+class Move():
+
+  def __init__(self, startPiece, endPiece):
+    
+    self.startPiece = startPiece
+    self.endPieve = startPiece
+    self.startPieceNotation = 
