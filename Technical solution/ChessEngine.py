01afb4d1ce60ca5653d383db4778ea16734b2269
Ben Pennycook <139886382+BenPenCoding@users.noreply.github.com>
2025-02-11 09:50:11 +0000

    Update ChessEngine.py
    Updated the Piece class and added more comments to simplify the code.

--- Technical solution/ChessEngine.py
+++ Technical solution/ChessEngine.py
@@ -2,12 +2,16 @@
 import pygame as pg
 
 #Classes
-class Piece():
+class Piece():		#A class that represents a chess piece with attributes such as piece type and piece location on the board
 	def __init__(self, pieceType, file, column):	#Constructor for the Piece class. Declares the:  
 		self.pieceType = pieceType              #type of piece (pawn, rook, etc)
 		self.file = file                        #file (row) in the board
 		self.column = column                 	#column in the board
 
+	#Functions
+	def findPossibleMoveLocations(self):	#An empty function that will be overridden in the piece subclasses, returns all locations that a piece can move to
+		None
+	
 	#Setters
 	def setFile(self, file):	#'Setter' for the file attribute, called when a piece is instantiated or moved
 		self.file = file
@@ -35,7 +39,7 @@ class Piece():
 	def getPieceType(self): 	#'Getter' for the pieceType attribute, returns said attribute when called
 		return self.pieceType
 
-class Move():
+class Move():	#A class that represents a chess move with attributes including the piece to move, and the piece for it to replace
 	def __init__(self, startPiece, endPiece):				#Constructor for the Move class, declares the:
 		pieceTypeDict = {"Knight":"N",					#dictionary that the program uses to determine the algebraic notation of a piece type
 				"Queen": "Q",					#
@@ -48,6 +52,17 @@ class Move():
 		self.endPiece = endPiece					#piece to replace with startPiece (could be a piece or an empty grid)
 		self.startPieceNotation = pieceTypeDict[self.startPiece]	#algebraic notation for piece to move
 		self.endPieceNotation = pieceTypeDict[self.endPiece]		#algebraic notation for piece to replace with startPiece
-	
-	
+
+	#Getters
+	def getStartPiece(self):	#'Getter' for the startPiece attribute, returns said attribute when called
+		return self.startPiece
+
+	def getEndPiece(self):		#'Getter' for the endPiece attribute, returns said attribute when called
+		return self.endPiece
+
+	def getStartPieceNotation(self):	#'Getter' for the startPieceNotation attribute, returns said attribute when called
+		return self.startPieceNotation
+
+	def getEndPieceNotation(self):		#'Getter' for the endPieceNotation attribute, returns said attribute when called
+		return self.endPieceNotation
 		
