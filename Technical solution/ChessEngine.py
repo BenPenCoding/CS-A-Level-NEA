37e8b33928bed7de53c059a13605fa4f97b118e0
Ben Pennycook <139886382+BenPenCoding@users.noreply.github.com>
2025-02-28 10:14:30 +0000

    Update ChessEngine.py
    Must finish Rook class, and add more comments to explain code

--- Technical solution/ChessEngine.py
+++ Technical solution/ChessEngine.py
@@ -1,5 +1,6 @@
 #Libraries
 import pygame as pg
+from time import *
 
 #Classes
 class Game():
@@ -36,6 +37,9 @@ class Piece():		#A class that represents a chess piece with attributes such as p
 	def setPieceType(self, pieceType):	#'Setter' for the pieceType attribute, called when a piece is instantiated or a pawn reaches the other side of the board and can change into another piece
 		self.pieceType = pieceType
 
+	def setPieceColour(Self, pieceColour):	#'Setter' for the pieceColour attribute, called when a piece is instantiated 
+		self.pieceColour = pieceColour
+	
 	#Getters
 	def getFile(self):	#'Getter' for the file attribute, returns said attribute when called
 		return self.file
@@ -49,8 +53,13 @@ class Piece():		#A class that represents a chess piece with attributes such as p
 	def getPieceType(self): 	#'Getter' for the pieceType attribute, returns said attribute when called
 		return self.pieceType
 
-	def isPieceEmpty(self):
-		if self.pieceType 
+	def getPieceColour(self):	#'Getter' for the pieceColour attribute, returns said attribute when called
+		return self.pieceType
+
+	#Checkers
+	def isPieceEmpty(self):		#'Checker' which returns the bool True if the value of the attribute pieceType is "Empty", meaning that the location that piece takes up is free for another piece to move to
+		if self.pieceType == "Empty":
+			return True
 
 class Rook(Piece): 			#A class that represents the rook chess piece, which inherits from the Piece class
 	def __init__(self):		#The constructor for the Rook class
@@ -62,15 +71,35 @@ class Rook(Piece): 			#A class that represents the rook chess piece, which inher
 		fileIncrement, columnIncrement = 0, 0  	#Declares two variables which will be used to change the direction of the board search by switching between -1 and 1 to reverse direction.
 		tempFile, tempColumn = file, column	#Declares two variables that store the temporary file and column of possible locations to move to
 		
-		for i in range(4):	#The four cardinal directions
-			while True:	#A while loop so that all possible move locations in a row are found 
-				
-				'''Add recoding to fileIncrement and columnIncrement to change direction each iteration'''
+		for i in range(4):		#The four cardinal directions
+			while True:		#A while loop so that all possible move locations in a row are found 
+				match i:	#Python's equivalent of a switch/case statement
+					case 0:		#If the increment variable has a value of 0, test all possible move locations to the right of the piece
+						fileIncrement, columnIncrement = 1, 0
+					case 1:		#If the increment variable has a value of 1, test all possible move locations above the piece
+						fileIncrement, columnIncrement = 0, -1
+					case 2:		#If the increment variable has a value of 2, test all possible move locations to the left of the piece
+						fileIncrement, columnIncrement = -1, 0
+					case 3:		#If the increment variable has a value of 3, test all possible move locations below the piece
+						fileIncrement, columnIncrement = 0, 1
+					case _:		#DEBUG - If the increment variable has a value not inside 0 <= i <= 3 then print the nature of the error
+						print("Fatal error, Rook class switch/case statement experienced a larger increment value than expected.")
+						sleep(5)	#DEBUG - Pauses the program for five seconds to allow the tester to interpret the error message 
+						quit()		#DEBUG - Quits the program entirely
+
+				if (tempFile > 7 or tempFile < 0) or (tempColumn > 7 or tempColumn < 0):	#Checks if the file and column of the next piece to check are actually not on the board and therefore if a "IndexError: list index out of range" error will occur                  
+					break									
 				
 				pieceToCheck = game.getPieceAtLocation(tempFile, tempColumn)	#Gets the piece object to check 
+				
 				if pieceToCheck.getPieceType() == "Empty":			#Checks if the piece object is empty
 					possibleMoveLocations.append([tempFile, tempColumn]) 	#If it is, add the location to the list of possible move locations
-				else:		#Otherwise the piece to check is not empty 
+				
+				elif (pieceToCheck.getPieceColour() != self.getPieceColour()) and pieceToCheck.getPieceType != "King":
+					possibleMoveLocations.append([tempFile, tempColumn])
+					break
+				
+				else:		#Otherwise the piece to check is the same colour as the piece to move, or it's the opposition's king piece (which can't be taken), or it's  
 					break	#Exits the while loop
 
 
